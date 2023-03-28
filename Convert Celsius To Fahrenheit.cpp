e3b74739d9818fc2be38f08cec64accc08587d0c
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-03-28 10:41:31 +0530

    Update Convert Celsius To Fahrenheit.cpp

--- Convert Celsius To Fahrenheit.cpp
+++ Convert Celsius To Fahrenheit.cpp
@@ -9,3 +9,17 @@ class Solution{
     return F;
     }
 };
+
+//OR
+
+class Solution{
+    public:
+    
+    //You need to complete this function
+    double cToF(int C)
+    {
+        //Your code here
+        int F = (C*9/5)+32;
+        return F;
+    }
+};
