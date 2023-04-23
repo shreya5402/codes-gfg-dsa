79c5b555230be2080e8bea8b5f9f3cf6fcb28ae8
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-04-23 17:16:30 +0530

    Update PowerOfNumber.cpp

--- Recursion/PowerOfNumber.cpp
+++ Recursion/PowerOfNumber.cpp
@@ -1,10 +1,10 @@
 //Given a number and its reverse. Find that number raised to the power of its own reverse. EXAMPLE:- Input:
-N = 2
-Output: 4
-Explanation: The reverse of 2 is 2
-and after raising power of 2 by 2 
-we get 4 which gives remainder as 
-4 by dividing 1000000007.//
+//N = 2
+//Output: 4
+//Explanation: The reverse of 2 is 2
+//and after raising power of 2 by 2 
+//we get 4 which gives remainder as 
+//4 by dividing 1000000007.
 
 class Solution{
     public:
