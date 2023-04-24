80a721454f479871c4d2644fa2768e03125b430a
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-04-24 20:04:01 +0530

    Update Fibonacci.cpp

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -4,7 +4,7 @@ class Solution{
     int fibonacci(int n)
     {
         //Your code here
-        if(n==1 || n==2) return 1;
+        if(n==1 || n==2) return 1; //or we can write if(n<=1) return n;
         return fibonacci(n-1)+ fibonacci(n-2);
     }
 };
